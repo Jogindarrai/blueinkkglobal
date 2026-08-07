beeab26eef1978590733bf2beca65fa5f8c60de7
Jogindar <jogindar.rw@gmail.com>
2026-08-07 17:53:20 +0530

    update

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,12 +4,20 @@ namespace App\Models;
 
 use Filament\Models\Contracts\FilamentUser;
 use Filament\Panel;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable implements FilamentUser
 {
-    use Notifiable;
+    use HasFactory, Notifiable, HasRoles;
+
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+    ];
 
     public function canAccessPanel(Panel $panel): bool
     {
